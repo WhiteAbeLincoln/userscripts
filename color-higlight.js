e95416d97e1698096b5f601dc9268f087b41615d
Abraham White <abelincoln.white@gmail.com>
2016-02-03 13:38:38 -0700

    used word boundries instead of whitespace

--- color-higlight.js
+++ color-higlight.js
@@ -10,7 +10,7 @@
 (function() {
 'use strict';
 
-var hex = new RegExp(/#([a-f\d]{6}|[a-f\d]{3})(?=[\s])?/ig);
+var hex = new RegExp(/#([a-f\d]{6}|[a-f\d]{3})\b/ig);
 var rgb = new RegExp(/rgb(?:a)?\((\b([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\b[, ]*){3,4}\)/ig);
 var span = document.createElement('span');
 
